10cd1f3768944516a0624adeafb5d7025fc4a6b4
Matthew Torres <matthew.torres211@gmail.com>
2023-09-19 02:24:33 +0800

    Add deployment to sepolia network

--- apps/web3/hardhat.config.ts
+++ apps/web3/hardhat.config.ts
@@ -1,14 +1,18 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+const { ALCHEMY_API_KEY, METAMASK_PRIVATE_KEY } = process.env
+
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
-  // gasReporter: {
-	// 	currency: 'USD',
-	// 	gasPrice: 100,
-	// 	enabled: true,
-	// 	maxMethodDiff: 10,
-	// },
+  defaultNetwork: "sepolia",
+	networks: {
+		hardhat: {},
+		sepolia: {
+			url: ALCHEMY_API_KEY,
+			accounts: [`0x${METAMASK_PRIVATE_KEY}`]
+		}
+	}
 };
 
 export default config;
--- apps/web3/scripts/deploy.ts
+++ apps/web3/scripts/deploy.ts
@@ -8,7 +8,7 @@ async function main() {
   const name = await mat.name();
   const address = await mat.getAddress()
 
-  console.log(`${name} has been deployed to ${address}.`)
+  console.log(`${name} has been deployed to ${address}`)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
@@ -17,3 +17,22 @@ main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
+
+// async function main() {
+//   const [deployer] = await ethers.getSigners();
+//   console.log("Deploying contracts with the account:", deployer.address);
+
+//   console.log(`Account balance: ${(await deployer.getBalance()).toString()}`);
+//   const TOR = await ethers.getContractFactory("TorNFT");
+
+//   const tor = await TOR.deploy();
+//   console.log(`Contract deployed to address: ${tor.address}`);
+// }
+
+// main()
+//   .then(() => process.exit(0))
+//   .catch((error) => {
+//     console.error(error);
+//     process.exit(1);
+//   });
